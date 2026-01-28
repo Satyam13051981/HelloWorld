191b8960746a1fd7aae08947925526f0d6220c20
Satyam13051981 <satyam2604can@gmail.com>
2026-01-28 11:24:29 +0530

    Multiply two numbers

---
+++ Multiply.java
@@ -0,0 +1,11 @@
+public class Multiply {
+    public static void main(String[] args) {
+        int x=8;
+        int y=9;
+
+        int result;
+        result=x*y;
+
+        System.out.println("Sum "+result);
+    }
+}
